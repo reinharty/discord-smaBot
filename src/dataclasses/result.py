5941637df5ae1104106e90790de780229078d836
reinharty <yorrick@aqube.de>
2025-02-28 18:52:02 +0100

    Add fields to Result

--- src/dataclasses/result.py
+++ src/dataclasses/result.py
@@ -2,7 +2,13 @@ from dataclasses import dataclass
 
 @dataclass
 class Result:
+    sma: int
     current_price: float
     yesterday_close: float
     current_sma: float
     yesterday_sma: float
+    #
+    current_diff: float
+    yesterday_diff: float
+    current_distance: float
+    yesterday_distance: float
