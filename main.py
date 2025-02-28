0d717be6691feae2fbfdbb0c0eb942da46848ad4
reinharty <yorrick@aqube.de>
2025-02-28 17:02:24 +0100

    refactor: main.py

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from datetime import datetime
 import discord
 import pycron
 from dotenv import load_dotenv
+import yfinance as yf
 
 from message import Message
-from scraper import Scraper
+from src.logic.scraper import Scraper
 
 intents = discord.Intents.default()
 intents.all()
@@ -19,6 +20,9 @@ scedule = os.getenv('SCEDULE')
 
 channel_sp500_alarm_id = int(os.getenv('SP500_ALARM_200'))
 channel_sp500_report_id = int(os.getenv('SP500_REPORT'))
+
+channel_sp500_190_alarm_id = int(os.getenv('SP500_ALARM_190'))
+#channel_sp500_190_report_id = int(os.getenv('DEBUG'))
 
 channel_nasdaq_alarm_id = int(os.getenv('NASDAQ_ALARM_220'))
 channel_nasdaq_report_id = int(os.getenv('NASDAQ_REPORT'))
@@ -34,15 +38,31 @@ channel_debug_id = int(os.getenv('DEBUG'))
 token = os.getenv('TOKEN')
 
 
-
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
     #schedule_daily_task()  # Schedule the daily message
     debug_channel = client.get_channel(channel_debug_id)
     await debug_channel.send("Online again at " + datetime.today().strftime("%Y-%m-%d-%H:%M:%S") + " " + scedule)
+    await debug_channel.send("yfinance version = " + yf.__version__)
+
+    channel = client.get_channel(channel_debug_id)
+    signal = Scraper.get_signal("^GDAXI", 200, 0.025)
+    message = Message().alarm_message('dax', signal)
+    await channel.send("debug " + message)
+
+    # Treasury Yield 30
+    # alarm
+    channel = client.get_channel(channel_debug_id)
+    signal = Scraper().get_signal("TLT", 60)
+    message = Message().alarm_message('tlt', signal)
+    await channel.send("debug " + message)
+
+
     #await jobs()
+    #await reports()
     await scheduler()  # Start the scheduler loop
+
 
 async def scheduler():
     """Run the scheduler loop to execute scheduled tasks."""
@@ -55,6 +75,7 @@ async def scheduler():
         else:
             await asyncio.sleep(15)
 
+
 async def jobs():
     # SP500
     # alarm
@@ -63,23 +84,18 @@ async def jobs():
     message = Message().alarm_message('sp500', signal)
     await channel.send(message)
 
-    # SP500
-    # report
-    channel = client.get_channel(channel_sp500_report_id)
-    message = Scraper().daily_report("^GSPC", 200)
-    await channel.send(message)
+    # SP500 190 SMA with 2.5% offset
+    # alarm
+    channel = client.get_channel(channel_sp500_190_alarm_id)
+    signal = Scraper().get_signal("^SP500TR", 190, 0.025)
+    message = Message().alarm_message('sp500', signal)
+    await channel.send(message + " ^SP500TR, SMA = 190, offset = 2.5%")
 
     # NASDAQ
     # alarm
     channel = client.get_channel(channel_nasdaq_alarm_id)
     signal = Scraper().get_signal("^NDX", 220)
     message = Message().alarm_message('nasdaq', signal)
-    await channel.send(message)
-
-    # NASDAQ
-    # report
-    channel = client.get_channel(channel_nasdaq_report_id)
-    message = Scraper().daily_report("^NDX", 220)
     await channel.send(message)
 
     # DAX
@@ -89,6 +105,27 @@ async def jobs():
     message = Message().alarm_message('dax', signal)
     await channel.send(message)
 
+    # Treasury Yield 30
+    # alarm
+    channel = client.get_channel(channel_tyx_alarm_id)
+    signal = Scraper().get_signal("TLT", 60)
+    message = Message().alarm_message('tlt', signal)
+    await channel.send(message)
+
+
+async def reports():
+    # SP500
+    # report
+    channel = client.get_channel(channel_sp500_report_id)
+    message = Scraper().daily_report("^GSPC", 200)
+    await channel.send(message)
+
+    # NASDAQ
+    # report
+    channel = client.get_channel(channel_nasdaq_report_id)
+    message = Scraper().daily_report("^NDX", 220)
+    await channel.send(message)
+
     # DAX
     # report
     channel = client.get_channel(channel_dax_report_id)
@@ -96,17 +133,11 @@ async def jobs():
     await channel.send(message)
 
     # Treasury Yield 30
-    # alarm
-    channel = client.get_channel(channel_tyx_alarm_id)
-    signal = Scraper().get_signal("^TYX", 60)
-    message = Message().alarm_message('tyx', signal)
-    await channel.send(message)
-
-    # Treasury Yield 30
     # report
     channel = client.get_channel(channel_tyx_report_id)
-    message = Scraper().daily_report("^TYX", 60)
+    message = Scraper().daily_report("TLT", 60)
     await channel.send(message)
+
 
 #load_dotenv()
 client.run(token)
