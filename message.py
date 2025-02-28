81835ba5f99b86f311aec1392492cb0b0a9d0f76
reinharty <yorrick@aqube.de>
2025-02-28 18:54:32 +0100

    Add message getter

--- message.py
+++ message.py
@@ -1,3 +1,6 @@
+from src.dataclasses.result import Result
+
+
 class Message:
     index_dict = {
         'sp500': ['S&P500', 'https://finance.yahoo.com/quote/%5EGSPC/chart/'],
@@ -18,3 +21,23 @@ class Message:
 
     def report_message(self, index, signal):
         return ''
+
+    @staticmethod
+    def get_position(result: Result)-> str:
+
+        position = "over" if result.current_price > result.current_sma else "under"
+        return f"Price is {result.current_price} and **{position}** SMA of {result.current_sma}.\n"
+
+    @staticmethod
+    def get_distances(result: Result)->str:
+
+        current_distance = "{:.2f}".format(result.current_distance)
+        yesterday_distance = "{:.2f}".format(result.yesterday_distance)
+
+        return f"Difference between today's closing price and {result.sma}-day SMA: {result.current_diff:.2f}\t**{current_distance}%**\nDifference between yesterday's closing price and {result.sma}-day SMA: {result.yesterday_diff:.2f}\t{yesterday_distance}%\n"
+
+    @staticmethod
+    def get_direction(result: Result):
+        gap_change_direction = "Increased" if abs(result.current_diff) > abs(result.yesterday_diff) else "Decreased"
+
+        return f"Gap between price and SMA since yesterday: **{gap_change_direction}**\n"
