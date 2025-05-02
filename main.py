f6d288b8c059c57656a83948d3c78e46439fc269
reinharty <yorrick@aqube.de>
2025-05-02 17:35:44 +0200

    Add dict with all channels of corresponding tickers

--- main.py
+++ main.py
@@ -37,6 +37,14 @@ channel_debug_id = int(os.getenv('DEBUG'))
 
 token = os.getenv('TOKEN')
 
+channels_of_tickers = {
+    "^GSPC" : [channel_sp500_alarm_id, channel_sp500_report_id],
+    "^SP500TR" : [channel_sp500_190_alarm_id, channel_sp500_190_report_id],
+    "nasdaq" : [channel_nasdaq_alarm_id, channel_nasdaq_report_id],
+    "^GDAXI" : [channel_dax_alarm_id, channel_dax_report_id],
+    "tlt" : [channel_tlt_alarm_id, channel_tlt_report_id]
+}
+
 
 @client.event
 async def on_ready():
